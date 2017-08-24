8b5fc27f4cf2aa4087a0a69e76b601d439ba586d
Jeroen Reumkens <jeroen.reumkens@tamtam.nl>
2017-08-24 13:31:47 +0200

    Added API util readme, and fixed getting of current environment

--- utilities/api/api.js
+++ utilities/api/api.js
@@ -1,3 +1,7 @@
+/**
+ * @shelf-version: 1.0.0
+ */
+
 import Environment from '../../system/environment';
 import $ from 'jquery';
 
@@ -23,23 +27,9 @@ class API {
 
     }
 
-    getEndoint(path, json, method) {
-
-        if ( path.substr(0, 4) === 'http' ) {
-            return path;
-        }
-
-        if ( json === true || (json === 'local' && Environment.isLocal()) ) {
-            return endpointBase.json + path + `--${method}.json`;
-        } else {
-            return endpointBase[ Environment.get() ] + path;
-        }
-
-    }
-
     get(path, data, json) {
 
-        return $.ajax( this.getEndoint(path, json, 'get'), {
+        return $.ajax( this._getEndoint(path, json, 'get'), {
             data,
             method: 'GET'
         } );
@@ -48,7 +38,7 @@ class API {
 
     post(path, data, json) {
 
-        return $.ajax( this.getEndoint(path, json, 'post'), {
+        return $.ajax( this._getEndoint(path, json, 'post'), {
             data,
             method: 'POST'
         } );
@@ -58,7 +48,7 @@ class API {
 
     put(path, data, json) {
 
-        return $.ajax( this.getEndoint(path, json, 'put'), {
+        return $.ajax( this._getEndoint(path, json, 'put'), {
             data,
             method: 'PUT'
         } );
@@ -67,10 +57,24 @@ class API {
 
     delete(path, data, json) {
 
-        return $.ajax( this.getEndoint(path, json, 'delete'), {
+        return $.ajax( this._getEndoint(path, json, 'delete'), {
             data,
             method: 'DELETE'
         } );
+
+    }
+
+    _getEndoint(path, json, method) {
+
+        if ( path.substr(0, 4) === 'http' || path.substr(0,1) === '?' ) {
+            return path;
+        }
+
+        if ( json === true || (json === 'local' && Environment.isLocal()) ) {
+            return endpointBase.json + path + `--${method}.json`;
+        } else {
+            return endpointBase[Environment.get()] + path;
+        }
 
     }
 
