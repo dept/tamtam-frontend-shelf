0f2b2f10fc6e712861049333bf6e0fb416593b40
Kees van Lierop <keesvlierop@gmail.com>
2017-09-21 18:05:34 +0200

    Consistent export declarations

--- utilities/validation/validation.js
+++ utilities/validation/validation.js
@@ -6,9 +6,9 @@ import luhn from "luhn";
  * @param  {String}  email Email address
  * @return {Boolean} TRUE if provided argument is a valid email address, FALSE if not
  */
-export const isValidEmail = (email) => {
+function isValidEmail(email) {
     return /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,10}))$/.test(email);
-};
+}
 
 /**
  * Check if string is valid phone number
@@ -16,9 +16,9 @@ export const isValidEmail = (email) => {
  * @param  {String} regex Default dutch phone regex
  * @return {Boolean} TRUE if provided argument is a valid phone number, FALSE if not
  */
-export const isValidPhoneNumber = (phone, regex = /^(^\+[0-9]{2}|^\+[0-9]{2}\(0\)|^\(\+[0-9]{2}\)\(0\)|^00[0-9]{2}|^0)([0-9]{9}$|[0-9\-\s]{10}$)$/) => {
+function isValidPhoneNumber(phone, regex = /^(^\+[0-9]{2}|^\+[0-9]{2}\(0\)|^\(\+[0-9]{2}\)\(0\)|^00[0-9]{2}|^0)([0-9]{9}$|[0-9\-\s]{10}$)$/) {
     return regex.test(phone);
-};
+}
 
 /**
  * Check if string is valid zipcode
@@ -26,24 +26,33 @@ export const isValidPhoneNumber = (phone, regex = /^(^\+[0-9]{2}|^\+[0-9]{2}\(0\
  * @param  {String} regex Default dutch zipcode regex
  * @return {Boolean} TRUE if provided argument is a valid zipcode, FALSE if not
  */
-export const isValidZipcode = (zipcode, regex = /^[1-9][0-9]{3} ?(?!sa|sd|ss)[a-z]{2}$/i) => {
+function isValidZipcode(zipcode, regex = /^[1-9][0-9]{3} ?(?!sa|sd|ss)[a-z]{2}$/i) {
     return regex.test(zipcode);
-};
+}
 
 /**
  * Check if string is valid IBAN number
  * @param  {String} IBAN Provided IBAN number
  * @return {Boolean} TRUE if provided argument is a valid IBAN number, FALSE if not
  */
-export const isValidIBAN = (IBAN) => {
+function isValidIBAN(IBAN) {
     return iban.isValid(IBAN);
-};
+}
 
 /**
  * Check if string complies with the Luhn algorith (used to validate credit card numbers or OV-chipkaart numbers)
  * @param  {Number|String} number provided input number
  * @return {Boolean} TRUE if provided argument is a valid Luhn number, FALSE if not
  */
-export const isValidLuhnNumber = (number) => {
+function isValidLuhnNumber(number) {
     return luhn.validate(number);
-};
+}
+
+
+export {
+    isValidEmail,
+    isValidPhoneNumber,
+    isValidZipcode,
+    isValidIBAN,
+    isValidLuhnNumber
+}
