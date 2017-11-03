e6109f9ec01adf5ee2ceddf01988aeded0860531
Adrian Klingen <adrian@tamtam.nl>
2017-11-03 12:53:00 +0100

    Added image component with lazyload && added image depedency to video.html

---
+++ components/image/javascript/image/lazy-image.js
@@ -0,0 +1,121 @@
+import 'core-js/fn/array/from';
+import 'core-js/fn/array/find';
+
+import Events from '../util/events';
+import Layzr from 'layzr.js';
+import parseSrcSet from './util/parse-srcset';
+import hasResponsiveImages from './util/detect-responsive-images';
+
+const SUPPORTS_SRCSET = checkResponsiveImageRequirements();
+
+class LazyImage {
+
+    constructor() {
+
+        this.instance = null;
+        this._bindEvents();
+
+    }
+
+    /**
+     * Bind generic events
+     */
+    _bindEvents() {
+
+        Events.$on('lazyimage::update', () => this._updateImages());
+        Events.$on('lazyimage::init', () => this._loadImages());
+
+    }
+
+    /**
+     * Lazy load all images if srcset is supported
+     */
+    _loadImages() {
+
+        if (!SUPPORTS_SRCSET) {
+            this.setTabletImage();
+            return;
+        }
+
+        this.instance = Layzr({
+            normal: 'data-src',
+            srcset: 'data-srcset',
+            threshold: 20
+        });
+
+        // Get all image data.
+        this.instance.update();
+
+        // Bind event listeners.
+        this.instance.handlers(true);
+
+        // Lazy load images that are already in view (before user starts scrolling).
+        this.instance.check();
+
+        // Retrigger objectfit polyfill after image is loaded.
+        this.instance.on('src:after', element => {
+            Events.$trigger('image::object-fit', element);
+        });
+
+    }
+
+    /**
+     * Update new images
+     */
+    _updateImages() {
+
+        if (!SUPPORTS_SRCSET) {
+            this.setTabletImage();
+            return;
+        }
+
+        // Get all image data.
+        this.instance.update();
+
+        // Bind event listeners.
+        this.instance.handlers(true);
+
+        // Lazy load images that are already in view (before user starts scrolling).
+        this.instance.check();
+
+    }
+
+    /**
+     * If srcset isn't supported set the default image size
+     */
+    _setTabletImage() {
+
+        const images = document.querySelectorAll('img');
+
+        Array.from(images).forEach(function (image) {
+
+            const srcSet = parseSrcSet(image.getAttribute('data-srcset')) || [{ url: image.getAttribute('data-src') }];
+
+            if (!srcSet) { return; }
+
+            const tablet = Array.from(srcSet).find((a) => a.width === 1024); // Pick tablet image.
+
+            const src = tablet ? tablet.url : srcSet[0].url;
+
+            image.src = null; // Setting to null first prevents weird bugs of not updating src in IE.
+            image.src = src;
+
+            image.removeAttribute('data-srcset');
+
+        });
+
+        Events.$trigger('image::object-fit');
+
+    }
+
+}
+
+/**
+ * Check if srcset is supported
+ * @returns {Boolean}
+ */
+function checkResponsiveImageRequirements() {
+    return hasResponsiveImages.srcset || hasResponsiveImages.picture;
+}
+
+export default new LazyImage();
---
+++ components/image/javascript/image/util/parse-srcset.js
@@ -0,0 +1,41 @@
+import numberIsNan from 'number-is-nan';
+
+function parseSrcSet(str) {
+
+    if ( !str ) {return false; }
+
+    return deepUnique(str.split(',').map(function (el) {
+        const ret = {};
+
+        el.trim().split(/\s+/).forEach(function (_el, i) {
+            if (i === 0) {
+                return ret.url = _el;
+            }
+
+            const value = _el.substring(0, _el.length - 1);
+            const postfix = _el[_el.length - 1];
+            const intVal = parseInt(value, 10);
+            const floatVal = parseFloat(value);
+
+            if (postfix === 'w' && /^\d+$/.test(value)) {
+                ret.width = intVal;
+            } else if (postfix === 'h' && /^\d+$/.test(value)) {
+                ret.height = intVal;
+            } else if (postfix === 'x' && !numberIsNan(floatVal)) {
+                ret.density = floatVal;
+            } else {
+                throw new Error('Invalid srcset descriptor: ' + _el + '.');
+            }
+        });
+
+        return ret;
+    }));
+}
+
+function deepUnique(arr) {
+    return arr.sort().filter(function (el, i) {
+        return JSON.stringify(el) !== JSON.stringify(arr[i - 1]);
+    });
+}
+
+export default parseSrcSet;
