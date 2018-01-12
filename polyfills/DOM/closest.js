45bbe6c24441664b69009cff489c5b96dab01659
Jeroen Reumkens <jeroen.reumkens@tamtam.nl>
2018-01-12 17:11:07 +0100

    Added closest and matches polyfills

---
+++ polyfills/DOM/closest.js
@@ -0,0 +1,25 @@
+/**
+ * Polyfill for element.closest
+ * @source: https://raw.githubusercontent.com/jonathantneal/closest/master/element-closest.js
+ */
+
+// closest requires element.matches.
+import './matches';
+
+(function (ElementProto) {
+	if (typeof ElementProto.closest !== 'function') {
+		ElementProto.closest = function closest(selector) {
+			var element = this;
+
+			while (element && element.nodeType === 1) {
+				if (element.matches(selector)) {
+					return element;
+				}
+
+				element = element.parentNode;
+			}
+
+			return null;
+		};
+	}
+})(window.Element.prototype);
