2253ef4de09a94f3fcb20fee1b29400d5703c172
Dylan Vens <dylan.vens@tamtam.nl>
2018-04-20 10:58:02 +0200

    Changed document.querySelector to childnodes and added header shadow functionality

--- components/header/javascript/header/header.js
+++ components/header/javascript/header/header.js
@@ -6,26 +6,27 @@ class Header {
     constructor(element) {
 
         this.element = element;
-        
+
         if (!this.element) { return; }
 
         this.height = 0;
         this.scrollValue = 0;
         this.prevScrollValue = 0;
         this.translateValue = 0;
+        this._initialized = false;
 
         this.bindEvents();
 
     }
 
     get fixed() {
-        
+
         return this.element.hasAttribute('fixed');
 
     }
 
     get condenses() {
-        
+
         return this.element.hasAttribute('condenses');
 
     }
@@ -36,10 +37,16 @@ class Header {
 
     }
 
+    get shadow() {
+
+        return this.element.hasAttribute('shadow');
+
+    }
+
     set shadow(val) {
 
         if (val) {
-            
+
             this.element.classList.add( HEADER_SHADOW_CLASS );
 
         } else {
@@ -55,14 +62,14 @@ class Header {
         return this.fixed ? this.scrollingDistanceHeight : this.height + 5;
 
     }
-    
+
     /**
      * Gets element with [sticky] attribute
      * Takes the first direct child when [sticky] attribute is not set
      */
     get stickyElement() {
 
-        const elements = this.element.querySelectorAll('div');
+        const elements = this.element.childNodes;
 
         let stickyElement;
 
@@ -76,7 +83,9 @@ class Header {
 
                     stickyElement = element;
 
-                } else if ( !stickyElement ) {
+                }
+
+                if ( !stickyElement ) {
 
                     stickyElement = element;
 
@@ -101,6 +110,16 @@ class Header {
             }
         ]);
 
+        if( !this._initialized ) {
+            this._scrollHandler();
+        }
+
+    }
+
+    isOnScreen() {
+
+        return this.height !== 0 && this.translateValue < this.height;
+
     }
 
     _scrollHandler() {
@@ -118,7 +137,7 @@ class Header {
     /**
      * Calculates the header translate value based on the options that are set
      */
-    
+
     _calculateHeaderTranslateValue() {
 
         const scrollDiff = this.scrollValue - this.prevScrollValue;
@@ -140,14 +159,16 @@ class Header {
         if ( this.scrollValue >= this.scrollingDistanceHeight ) {
 
             this.translateValue = this.condenses && !this.fixed ? Math.max(this.scrollingDistanceHeight, this.translateValue) : this.translateValue;
-    
+
         }
-        
+
+        this.isScrollingDown = this.scrollValue > this.prevScrollValue;
         this.prevScrollValue = this.scrollValue;
 
         if( this.scrollValue <= 0 ) {
 
             this._transformHeader( 0 );
+
 
         } else {
 
@@ -155,17 +176,45 @@ class Header {
 
         }
 
+        if( this.shadow ) {
+
+            this._applyShadow();
+
+        }
+
+    }
+
+    _applyShadow() {
+
+        if( this.fixed ) {
+
+            this.element.classList.add( HEADER_SHADOW_CLASS );
+
+        }
+
+        if( this.scrollValue <= 0 || !this.isOnScreen() ) {
+
+            this.element.classList.remove( HEADER_SHADOW_CLASS );
+
+        }
+
+        if( this.isOnScreen() && this.scrollValue > 0 && !this.isScrollingDown ) {
+
+            this.element.classList.add( HEADER_SHADOW_CLASS );
+
+        }
+
     }
 
     /**
      * Applies the calculated value from _calculateHeaderTranslateValue to the header
-     * and applies it to the sticky element when a sticky element is available 
+     * and applies it to the sticky element when a sticky element is available
      */
 
     _transformHeader( value ) {
 
         this.element.style.transform = `translate3d(0, -${value}px, 0)`;
-        
+
         if ( this.stickyElement ) {
 
             this.stickyElement.style.transform = `translate3d(0, ${ (Math.min(value, this.scrollingDistanceHeight) - this.stickyElementTop) }px, 0)`;
@@ -181,6 +230,8 @@ class Header {
             }
 
         }
+
+        this._initialized = true;
 
     }
 
