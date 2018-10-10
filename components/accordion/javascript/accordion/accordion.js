7899d6d07debf3a6e5ea29acf4b5d7f6a79cd670
daphnes <daphne@tamtam.nl>
2018-10-10 13:21:56 +0200

    Fix multiple accordions on one page and old way of opening events by id

--- components/accordion/javascript/accordion/accordion.js
+++ components/accordion/javascript/accordion/accordion.js
@@ -1,50 +1,28 @@
-import Events from '@utilities/events';
-import AccordionItem from './accordion-item';
+import Events from "@utilities/events";
+import AccordionItem from "./accordion-item";
 
-const ACCORDION_ITEM_HOOK = '[js-hook-accordion-item]';
+const ACCORDION_ITEM_HOOK = "[js-hook-accordion-item]";
 
 class Accordion {
-
     constructor(element) {
-
         this.element = element;
         this.items = this.element.querySelectorAll(ACCORDION_ITEM_HOOK);
 
-        if (!this.items) { return; }
+        if (!this.items) {
+            return;
+        }
 
-        this.autoClose = this.element.dataset.autoclose !== 'false';
+        this.autoClose = this.element.dataset.autoclose !== "false";
         this.accordionItems = {};
         this._initItems();
-        this._bindEvents();
-
     }
 
     /**
-     * Bind all events
+     * Bind all accordion specific events
+     * @param {HTMLElement} item  | accordion item
      */
-    _bindEvents() {
-
-        Events.$on(`accordion::animating`, (event, data) => {
-            if (this.accordionItems[data.id]) {
-                this.isAnimating = data.animating;
-            }
-        });
-
-        Events.$on(`accordion::toggle`, (event, id) => {
-            if (id && this.accordionItems[id] && !this.isAnimating) {
-                this._closeAllChildren(id);
-                this.accordionItems[id].toggle();
-            }
-        });
-
-        Events.$on(`accordion::open`, (event, id) => {
-            if (id && this.accordionItems[id] && !this.isAnimating) {
-                this._closeAllChildren(id);
-                this.accordionItems[id].open();
-            }
-        });
-
-        Events.$on(`accordion::close`, (event, id) => {
+    _bindAccordionEvents({ id }) {
+        Events.$on(`accordion[${id}]::close`, () => {
             if (id) {
                 if (this.accordionItems[id] && !this.isAnimating) {
                     this.accordionItems[id].close();
@@ -55,21 +33,35 @@ class Accordion {
                 });
             }
         });
+        Events.$on(`accordion[${id}]::open`, () => {
+            if (id && this.accordionItems[id] && !this.isAnimating) {
+                this._closeAllChildren(id);
+                this.accordionItems[id].open();
+            }
+        });
 
+        Events.$on(`accordion[${id}]::toggle`, () => {
+            if (id && this.accordionItems[id] && !this.isAnimating) {
+                this._closeAllChildren(id);
+                this.accordionItems[id].toggle();
+            }
+        });
+        Events.$on(`accordion[${id}]::animating`, (event, data) => {
+            if (this.accordionItems[data.id]) {
+                this.isAnimating = data.animating;
+            }
+        });
     }
 
     /**
      * Iterate over each item inside the accordion
      */
     _initItems() {
-
         Array.from(this.items).forEach(el => {
-
             const item = new AccordionItem(el);
             this.accordionItems[item.id] = item;
-
+            this._bindAccordionEvents(this.accordionItems[item.id]);
         });
-
     }
 
     /**
@@ -77,16 +69,17 @@ class Accordion {
      * @param {String} skipId Identifier which should be skipped
      */
     _closeAllChildren(skipId) {
-
-        if (!this.autoClose) { return; }
+        if (!this.autoClose) {
+            return;
+        }
 
         Object.keys(this.accordionItems).forEach(id => {
-            if (skipId === id) { return; }
+            if (skipId === id) {
+                return;
+            }
             this.accordionItems[id].close();
         });
-
     }
-
 }
 
 export default Accordion;
