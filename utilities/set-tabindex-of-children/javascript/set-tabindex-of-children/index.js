903117dd3310bf41894fde0fb592139c26d75d4c
Adrian Klingen <adrian@tamtam.nl>
2018-11-16 16:35:59 +0100

    Added customSelector and overwriteDefault

--- utilities/set-tabindex-of-children/javascript/set-tabindex-of-children/index.js
+++ utilities/set-tabindex-of-children/javascript/set-tabindex-of-children/index.js
@@ -1,3 +1,5 @@
-export default function setTabIndexOfChildren(element, value) {
-    [...element.querySelectorAll('a, area, input:not([disabled]), select:not([disabled]), textarea:not([disabled]), button:not([disabled]), iframe, video')].forEach(element => element.tabIndex = value);
+export const setTabIndexOfChildren = (element, tabIndexValue, customSelector = '', overwriteDefault = false) {
+    const defaultSelector = overwriteDefault ? '' : 'a, area, input:not([disabled]), select:not([disabled]), textarea:not([disabled]), button:not([disabled]), iframe, video';
+    const selector = overwriteDefault && customSelector.length ? customSelector : `${defaultSelector}${customSelector.length ? `,${customSelector}` : ''}`;
+    [...element.querySelectorAll(selector)].forEach(element => { element.tabIndex = tabIndexValue });
 }
