2a203c6e5f1e49dcd8955ff6a1e9df3baa6d19fc
daphnes <daphne@tamtam.nl>
2019-05-08 15:56:57 +0200

    Refactor component

--- utilities/store/index.js
+++ utilities/store/index.js
@@ -1,10 +1,10 @@
 import actions from './store/actions';
 import mutations from './store/mutations';
-import state from './store/state';
+import initialState from './store/state';
 import Store from './store/store';
 
 export default new Store({
   actions,
   mutations,
-  state
+  initialState
 });
--- utilities/store/lib/component.js
+++ utilities/store/lib/component.js
@@ -1,16 +1,21 @@
 import Store from '../store/store';
 
 class Component {
-    constructor(props = {}) {
-        this.render = this.render || function() {};
+  constructor(props = {}) {
+    this.render = this.render || function() {};
 
-        if (props.store instanceof Store) {
-            props.store.events.subscribe('stateChange', () => this.stateChanged());
+    if (props.store instanceof Store) {
+      // This fires every time the state updates
+      props.store.subscribe(state => {
+        if (typeof this.stateChanged === 'function') {
+          this.stateChanged(state);
         }
-
-        if (props.element) {
-            this.element = props.element;
-        }
+      });
     }
+
+    if (props.element) {
+      this.element = props.element;
+    }
+  }
 }
 export default Component;
